fda477e867bd186f256855a4ba8f57e7cf2bf63d
tlanders <tlanders@landersconsulting.biz>
2017-05-02 22:22:37 -0500

    files from spring start

---
+++ springboot/restsample/src/main/java/lci/biz/springboot/RestsampleApplication.java
@@ -0,0 +1,12 @@
+package lci.biz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestsampleApplication.class, args);
+	}
+}
