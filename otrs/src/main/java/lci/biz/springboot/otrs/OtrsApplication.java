a978653f862b230d36021f460ac57af06e74d01e
tlanders <tlanders@landersconsulting.biz>
2017-05-08 22:31:14 -0500

    start of otrs app

---
+++ otrs/src/main/java/lci/biz/springboot/otrs/OtrsApplication.java
@@ -0,0 +1,12 @@
+package lci.biz.springboot.otrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtrsApplication.class, args);
+	}
+}
