3f8ff9bdd41caef7c8144818da244622172add21
tlanders <tlanders@landersconsulting.biz>
2017-05-11 22:46:23 -0500

    added findByName rest call

--- otrs/src/main/java/lci/biz/springboot/otrs/controller/RestaurantController.java
+++ otrs/src/main/java/lci/biz/springboot/otrs/controller/RestaurantController.java
@@ -1,10 +1,17 @@
 package lci.biz.springboot.otrs.controller;
 
+import lci.biz.springboot.otrs.model.Restaurant;
 import lci.biz.springboot.otrs.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -20,5 +27,21 @@ public class RestaurantController {
     @Autowired
     public RestaurantController(RestaurantService restaurantService) {
         this.restaurantService = restaurantService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Collection<Restaurant>> findByName(@RequestParam("name") String name) {
+        logger.info("findByName, name=" + name);
+        Collection<Restaurant> restaurants;
+        try {
+            restaurants = restaurantService.findByName(name.toLowerCase());
+        } catch(Exception ex)  {
+            logger.log(Level.SEVERE, "Exception calling findByName, name=" + name, ex);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return (restaurants != null && restaurants.size() > 0)
+                ? new ResponseEntity<>(restaurants, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
